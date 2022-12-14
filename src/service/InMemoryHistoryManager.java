fa7de3c0ce51cef5318b11d6d23ab0dd16714edd
Vyacheslav <torlant312@yandex.ru>
2022-12-14 11:50:59 +0300

    feat: добавить remove() в InMemoryHistoryManager

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -18,6 +18,11 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     @Override
+    public void remove(int id) {
+        browsingHistory.removeIf(task -> task.getId() == id);
+    }
+
+    @Override
     public List<Task> getHistory() {
 
         return browsingHistory;
