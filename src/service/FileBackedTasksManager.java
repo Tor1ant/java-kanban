c7368664e42617721492199d3eb0be143fc7a10b
Vyacheslav <torlant312@yandex.ru>
2023-01-03 14:08:37 +0300

    feat: добавить SaveData

---
+++ src/service/FileBackedTasksManager.java
@@ -0,0 +1,6 @@
+package service;
+
+public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
+
+
+}
--- src/service/InMemoryTaskManager.java
+++ src/service/InMemoryTaskManager.java
@@ -3,7 +3,6 @@ package service;
 import model.Epic;
 import model.SubTask;
 import model.Task;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
