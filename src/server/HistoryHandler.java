fa69567330aa62b996fcd3f4ec9bd66cd94e1ec6
Vyacheslav <torlant312@yandex.ru>
2023-02-04 14:08:42 +0300

    feat: реализовать эндпоинт /tasks/history

---
+++ src/server/HistoryHandler.java
@@ -0,0 +1,28 @@
+package server;
+
+import com.sun.net.httpserver.HttpExchange;
+import service.FileBackedTasksManager;
+
+public class HistoryHandler extends PrioritizedTasksHandler {
+    public HistoryHandler(FileBackedTasksManager fileBackedTasksManager) {
+        super(fileBackedTasksManager);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) {
+        String requestMethod = exchange.getRequestMethod();
+        if (requestMethod.equals("GET")) {
+            historyHandle(exchange);
+        } else
+            writeResponse(exchange, "Данный метод не поддерживается.", 405);
+
+    }
+
+    private void historyHandle(HttpExchange exchange) {
+        if (exchange.getRequestURI().getQuery() == null) {
+            String historyInJson = gson.toJson(fileBackedTasksManager.getHistory());
+            writeResponse(exchange, historyInJson, 200);
+        } else
+            writeResponse(exchange, "у данного запроса не может быть параметров", 400);
+    }
+}
--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -8,15 +8,35 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class HttpTaskServer {
-    public static void main(String[] args) {
-        HttpServer httpServer;
+    int PORT = 8080;
+    private final HttpServer httpServer;
+    FileBackedTasksManager fileBackedTasksManager;
 
+    public HttpTaskServer() throws IOException {
+        fileBackedTasksManager = Managers.getDefaultFileBacked();
+        httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/tasks/task", new TaskHandler(fileBackedTasksManager));
+        httpServer.createContext("/tasks", new PrioritizedTasksHandler(fileBackedTasksManager));
+        httpServer.createContext("/tasks", new HistoryHandler(fileBackedTasksManager));
+        httpServer.createContext("/tasks/history", new HistoryHandler(fileBackedTasksManager));
+    }
+
+    public void startServer() {
+        System.out.println("Сервер начал работу на " + PORT);
+        System.out.println("http://localhost:" + PORT + "/tasks");
+        httpServer.start();
+    }
+
+    public void stopServer() {
+        System.out.println("Сервер прекратил работу " + PORT);
+        httpServer.stop(0);
+    }
+
+    public static void main(String[] args) {
         try {
-            httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
-            FileBackedTasksManager fileBackedTasksManager = Managers.getDefaultFileBacked();
-            httpServer.createContext("/tasks/task", new TaskHandler(fileBackedTasksManager));
-            httpServer.createContext("/tasks",new PrioritizedTasksHandler(fileBackedTasksManager));
-            httpServer.start();
+            HttpTaskServer taskServer = new HttpTaskServer();
+            taskServer.startServer();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
